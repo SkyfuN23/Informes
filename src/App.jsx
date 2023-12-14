7c1dad52a51516f45d059b650971af56ccca2f13
Pipas <damianrmas@hotmail.com>
2023-12-14 19:49:43 -0300

    hola

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,21 @@
-import React from 'react';
-import { useState } from 'react';
-import './App.css';
-import { TaskForm } from './components/TaskForm/TaskForm';
-import { TablaNoInformados } from './components/TablaNoInformados/TablaNoInformados';
-import { TablaInformados } from './components/TablaInformados/TablaInformados';
-import { Informe } from './components/Informe/Informe';
+import React, { useState } from "react";
+import "./App.css";
+import { Informe } from "./components/Informe/Informe";
+import { TablaInformados } from "./components/TablaInformados/TablaInformados";
+import { TablaNoInformados } from "./components/TablaNoInformados/TablaNoInformados";
+import { TaskForm } from "./components/TaskForm/TaskForm";
 
 function App() {
-
   const [pacientes, setPacientes] = useState([]);
 
   return (
     <>
       <body>
-        <h1 className='titulo'>Informes RX</h1>
-        <TaskForm
-          setPacientes={setPacientes}
-          pacientes={pacientes}
-        />
-        <div className='div-contenedor-listas'>
-          <TablaNoInformados
-            setPacientes={setPacientes}
-            pacientes={pacientes}
-          />
-          <TablaInformados
-            setPacientes={setPacientes}
-            pacientes={pacientes}
-          />
+        <h1 className="titulo">Informes RX</h1>
+        <TaskForm setPacientes={setPacientes} pacientes={pacientes} />
+        <div className="div-contenedor-listas">
+          <TablaNoInformados setPacientes={setPacientes} pacientes={pacientes} />
+          <TablaInformados setPacientes={setPacientes} pacientes={pacientes} />
         </div>
         <Informe />
       </body>
